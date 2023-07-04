de3cefd0087ec6d4117af47266160007949ef227
dabao <1193351076@qq.com>
2023-07-04 14:54:41 +0800

    first-commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/***
+ * Author: DaBao
+ * Date: ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
